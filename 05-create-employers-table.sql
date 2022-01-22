e07097718a178f0e9c752e309674e6f4b7a07522
jspark <sylvan0212@gmail.com>
2022-01-22 16:21:07 +0900

    48

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE employers (
     company_name VARCHAR(250),
-    company_address VARCHAR(300),
-    --yearly_revenue FLOAT(5, 2 ) -- Approximation
-    yearly_revenue NUMERIC(5, 2) -- Exact value    
+    company_address VARCHAR(300),    
+    yearly_revenue NUMERIC(5, 2),
+    is_hiring BOOLEAN
 );
