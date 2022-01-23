92c38065b9de3537ed23812d620f59b8f3e634a9
jspark <sylvan0212@gmail.com>
2022-01-23 10:17:46 +0900

    50

---
+++ 10-insert-data-again.sql
@@ -0,0 +1,11 @@
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+VALUES ('Microsoft', 'USA', 30, TRUE)
+
+INSERT INTO employers (company_name, company_address, yearly_revenue)
+VALUES ('Google', 'USA', 20)
+
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+VALUES ('Max Schwarz', 'Microsoft', 'Hello!', '2022-01-22')
+
+INSERT INTO conversations (user_name, employer_name, message)
+VALUES ('JS', 'Google', 'Hi!')
